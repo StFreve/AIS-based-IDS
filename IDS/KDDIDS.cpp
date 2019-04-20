493a257c87c282a0f151076250077959f1485307
StFreve <golfik2007@gmail.com>
2019-04-20 11:43:44 +0400

    Implement KDDIDS main functionality

--- IDS/KDDIDS.cpp
+++ IDS/KDDIDS.cpp
@@ -1,12 +1,16 @@
 #include "KDDIDS.h"
 #include "AIS/Algorithms.h"
+#include "AIS/KDDReader.h"
+
 #include <algorithm>
+#include <iostream>
+
 namespace IDS
 {
 
 KDDIDS::KDDIDS(const std::string & kdd_training_set, const std::string & kdd_full_set)
 	: kdd_training_set_(kdd_training_set)
-	, kdd_full_set_(kdd_full_set_)
+	, kdd_full_set_(kdd_full_set)
 {
 }
 
@@ -16,6 +20,8 @@ KDDIDS::~KDDIDS()
 
 void KDDIDS::start()
 {
+	training();
+	emulate_activity();
 }
 
 void KDDIDS::training()
@@ -38,6 +44,30 @@ void KDDIDS::training()
 
 void KDDIDS::emulate_activity()
 {
+	AIS::KDDReader kdd_reader(kdd_full_set_);
+
+	auto antigens = kdd_reader.read_chunk(200);
+	size_t errors[4];
+	for (const auto& antigen : antigens) {
+		for (const auto& detector : detectors_) {
+			if (detector->match(antigen.first.get()) && antigen.second) {
+				++errors[0];
+			}
+			else if (detector->match(antigen.first.get()) && !antigen.second) {
+				++errors[1];
+			}
+			else if (!detector->match(antigen.first.get()) && antigen.second) {
+				++errors[2];
+			}
+			else if (!detector->match(antigen.first.get()) && !antigen.second) {
+				++errors[3];
+			}
+		}
+	}
+	std::cout << "Correctly recognized as victim: " << errors[1] << std::endl;
+	std::cout << "Correctly recognized as legit: " << errors[2] << std::endl;
+	std::cout << "Incorrectly recognized as victim: " << errors[0] << std::endl;
+	std::cout << "Inorrectly recognized as legit: " << errors[3] << std::endl;
 }
 
 }
